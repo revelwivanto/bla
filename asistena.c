8ee9fd3da5e7ddb80d6d4bf5691b6c3db422cb28
revelwivanto <116476269+revelwivanto@users.noreply.github.com>
2025-04-30 19:45:05 +0700

    Update asistena.c

--- asistena.c
+++ asistena.c
@@ -5,11 +5,15 @@
 #include <unistd.h>
 
 int main(){
-    int id=shmget(1234,512,0666);
-    char*ptr=(char*)shmat(id,0,0);
-    while(ptr[0]==0)usleep(100000);
-    printf("Praktikan: %s\n",ptr);
-    strcpy(ptr+256,"Boleh, nanti malam ya");
+    int id = shmget(1234, 512, 0666 | IPC_CREAT);
+    char *ptr = (char*)shmat(id, NULL, 0);
+
+    while (ptr[0] == 0) usleep(100000);
+    printf("asisten: %s\n", ptr);
+
+    strcpy(ptr + 256, "Boleh, nanti malam ya");
+
     shmdt(ptr);
+    shmctl(id, IPC_RMID, 0);
     return 0;
 }
