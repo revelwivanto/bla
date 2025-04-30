3413215a9a3be59504b9b2130aded71291457143
revelwivanto <116476269+revelwivanto@users.noreply.github.com>
2025-04-30 19:43:33 +0700

    Update asisten.c

--- asisten.c
+++ asisten.c
@@ -5,11 +5,11 @@
 
 int main(){
     key_t key=1234;
-    int id=shmget(key,256,0666);
-    char*ptr=(char*)shmat(id,NULL,0);
-    while(ptr[0]=='\0')usleep(100000);
+    int id=shmget(key,256,0666|IPC_CREAT);
+    char* ptr=(char*)shmat(id,NULL,0);
+    while(ptr[0]==0) usleep(100000);
     printf("Asisten: %s\n",ptr);
     shmdt(ptr);
-    shmctl(id,IPC_RMID,NULL);
+    shmctl(id,IPC_RMID,0);
     return 0;
 }
