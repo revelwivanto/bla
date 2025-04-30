9759708640f0c9664fdfd56431dc0a62738cda33
revelwivanto <116476269+revelwivanto@users.noreply.github.com>
2025-04-30 19:36:46 +0700

    Update praktikana.c

--- praktikana.c
+++ praktikana.c
@@ -4,14 +4,15 @@
 #include <string.h>
 #include <unistd.h>
 
-int main(int argc,char*argv[]){
-    int id=shmget(1234,512,0666|IPC_CREAT);
-    char*ptr=(char*)shmat(id,0,0);
-    if(argc>1)strcpy(ptr,argv[1]);
-    printf("praktikan: %s\n",ptr);
-    while(ptr[256]==0)usleep(100000);
-    printf("praktikan: %s\n",ptr+256);
+int main(int argc, char* argv[]) {
+    int id = shmget(1234, 512, 0666 | IPC_CREAT);
+    char* ptr = (char*) shmat(id, 0, 0);
+    memset(ptr, 0, 512);
+    if (argc > 1) strcpy(ptr, argv[1]);
+    printf("praktikan: %s\n", ptr);
+    while (ptr[256] == 0) usleep(100000); 
+    printf("praktikan: %s\n", ptr + 256);
     shmdt(ptr);
-    shmctl(id,IPC_RMID,0);
+    shmctl(id, IPC_RMID, 0); 
     return 0;
 }
