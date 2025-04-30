78d1b9feaa116dbdf142345b321cbe3a43fb0fc1
revelwivanto <116476269+revelwivanto@users.noreply.github.com>
2025-04-30 19:43:14 +0700

    Update praktikan.c

--- praktikan.c
+++ praktikan.c
@@ -1,25 +1,14 @@
 #include <stdio.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
-#include <unistd.h>
 #include <string.h>
 
 int main(){
-    key_t key = 1234;
-    int id = shmget(key, 256, 0666);
-    char* ptr = (char*) shmat(id, NULL, 0);
-
-    char last[256] = {0}; 
-
-    while(1){
-        if(ptr[0] != '\0' && strcmp(ptr, last) != 0){
-            printf("Asisten: %s\n", ptr);
-            strcpy(last, ptr); 
-        }
-        usleep(100000); 
-    }
-
+    key_t key=1234;
+    int id=shmget(key,256,0666|IPC_CREAT);
+    char* ptr=(char*)shmat(id,NULL,0);
+    memset(ptr,0,256);
+    strcpy(ptr,"Halo mas, bisa demo kapan?");
     shmdt(ptr);
-    shmctl(id, IPC_RMID, NULL);
     return 0;
 }
